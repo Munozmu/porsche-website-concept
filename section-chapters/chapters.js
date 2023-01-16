0175f2807bc7b3c4e7333a6f90b4707b34d3048a
Gabin Munoz <32036115+Munozmu@users.noreply.github.com>
2023-01-16 23:24:19 +0100

    create data for chapter section

---
+++ section-chapters/chapters.js
@@ -0,0 +1,97 @@
+/** 
+ * Document contenant les chapitres et sous chapitres de la section de présentation.
+ */
+
+const datas = [
+    {
+        subject: "Aérodynamisme et design",
+        title: "La forme suit le sens du vent.",
+        description: "Le vent de face est un défi majeur pour améliorer les temps au tour. La nouvelle 911 GT3 RS optimise le flux avec chaque composant. Pour garantir un coefficient de traînée minimum, la partie arrière est en deux éléments, avec sorties d’air agrandies, une ventilation derrière l’aileron arrière et un diffuseur.",
+
+        stories: [
+            {
+                title: "Appui optimisé",
+                imgPath: "img/section-chapters/chapters/1.jpg",
+                description: 'La nouvelle 911 GT3 RS devrait s’adjuger des records au tour, grâce à un aérodynamisme largement révisé et un appui renforcé. Le système Porsche Active Aerodynamics (PAA) avec DRS utilise toutes les données du véhicule, comme l’accélération, la position de la pédale d’accélérateur et de frein, pour adapter automatiquement et rapidement la position de l’aileron arrière, ainsi que celle du diffuseur avant réglable selon la situation.'
+            },
+            {
+                title: '+40kg d\'appui.',
+                imgPath: '',
+                description: 'Les nouvelles barres aérodynamiques sur l’essieu avant augmentent la force d’appui (jusqu’à 40 kg à vitesse maximale) en réduisant les turbulences et la vitesse d’écoulement derrière les barres. Une technologie de pointe issue du sport automobile.'
+            },
+            {
+                title: 'Carrosserie élargie type Turbo avec entrées d\'air latérales. ',
+                imgPath: '',
+                description: 'La carrosserie élargie type Turbo avec grilles d’entrée d’air contribue de manière significative à l’optimisation aérodynamique de la 911 GT3 RS et renforce son allure athlétique. Elle permet également de loger de nouvelles roues plus larges qui font progresser l’agilité du véhicule. ',
+            },
+        ]
+    },
+    {
+        subject: 'Structure allégée',
+        title: 'Étonnante de légèreté.',
+        description: 'Réduire le poids, au maximum. La nouvelle 911 GT3 RS reprend ce principe à son compte, malgré une dotation plus riche et plus de performances par rapport à ses devancières (aérodynamisme actif, roues et pneus plus larges, carrosserie élargie et nouvelles options d’ajustement du châssis dans différents modes de conduite). ',
+
+        stories: [
+            {
+                title: "Pack Weissach - Extérieur",
+                imgPath: '',
+                description: "Dans la course au millième de seconde, chaque gramme compte. Le pack Weissach en option réduit le poids d'environs 15 kg. Le capot avant, le toit et les coques des rétroviseurs en carbone apparent (finition brillante) accentuent visuellement le caractère sportif de la nouvelle 911 GT3 RS, tout comme l’aileron arrière, en partie en carbone apparent (finition brillante), ainsi que l' inscription « PORSCHE »."
+            },
+            {
+                title: "Pack Weissach - Intérieur",
+                imgPath: "À l’intérieur, l’arceau de sécurité arrière en carbone apparent (finition satinée) rappelle l’ambition du modèle, et assure une protection supplémentaire sur circuit. L’optimisation se poursuit avec les palettes de changement de vitesse PDK ultralégères en magnésium, le dessus de la planche de bord en Race-Tex Noir, les éléments intérieurs en carbone apparent (finition satinée), une plaque avec le logo « Weissach RS » côté passager et des appuie-tête au logo « Weissach RS » brodé. ",
+                description: ''
+            },
+            {
+                title: "Pack Weissach - Châssis",
+                imgPath: "Également en PRFC, les stabilisateurs des essieux avant et arrière, les barres d’accouplement sur l’essieu arrière et la plaque de cisaillement contribuent à atteindre un poids idéal pour de nouveaux records. ",
+                description: ''
+            },
+        ]
+    },
+    {
+        subject: "Performances",
+        title: "Performances maximales. ",
+        description: "Dès que le feu passe au vert, la nouvelle 911 GT3 RS et son couple maxi de 465 Nm fournissent des performances incroyables sur circuit. Ses 525 ch (386 kW) vous catapultent de 0 à 100 km/h en 3,2 secondes : aucun répit jusqu’à la vitesse de pointe sur circuit de 296 km/h. Le système d’échappement sport léger en acier inoxydable garantit une sonorité brute jusqu’au régime maxi de 9 000 tr/min. ",
+        stories: [
+            {
+                title: "Anti-Dive amélioré.",
+                description: "Tout conducteur ambitieux sait qu’une décélération précise compte autant qu’une accélération puissante. Pour améliorer l’équilibre aérodynamique au freinage, la géométrie optimisée du châssis réduit la déviation de l’essieu avant, renforçant ainsi l’action « anti-plongée ». ",
+                imgPath: ""
+            },
+            {
+                title: "Dynamique transversale.",
+                description: "La nouvelle 911 GT3 RS réduit au minimum les freinages nécessaires avant les virages et chicanes. Les pneus sport 275/35 R20 à l’avant et 335/30 R21 à l’arrière, les nouvelles jantes et les voies élargies optimisent la dynamique transversale et la vitesse de passage en courbe.",
+                imgPath: ""
+            },
+            {
+                title: "Encore plus robuste.",
+                description: "En course, l’homme et la machine donnent tout. La boîte PDK (Porsche Doppelkupplung) à 7 rapports adaptée pour la compétition est encore plus robuste sur circuit, grâce à un système de ventilation modifié avec rapports de transmission ajustés. ",
+                imgPath: ""
+            }
+
+        ]
+    },
+    {
+        subject: "Intérieur et technologies",
+        title: "La victoire est un état d’esprit.",
+        description: "Avec 4 sélecteurs de mode intégrés, le volant typé sport automobile offre des possibilités de réglage inédites : DRIVE MODE, PASM (Porsche Active Suspension Management), PTV Plus (Porsche Torque Vectoring Plus), mais aussi Traction Control (TC). Le pilote fait encore mieux corps avec son véhicule. ",
+        stories: [
+            {
+                title: "À toute épreuve.",
+                description: "Le pack Clubsport, disponible sans coût supplémentaire, ajoute un arceau de sécurité en acier derrière les sièges avant, boulonné à la carrosserie et peint au choix en Noir. Un harnais 6 points, contribue également à renforcer la sécurité du pilote. ",
+                imgPath: ""
+            },
+            {
+                title: "Confort renforcé.",
+                description: "Les sièges baquets en matière plastique renforcée en fibre de carbone (PRFC) en carbone apparent avec un airbag de thorax intégré offrent un maintien latéral particulièrement efficace pour un poids réduit. Ils disposent d’un réglage manuel pour l’avancée des sièges et d’un réglage électrique pour leur hauteur. Le revêtement est en cuir Noir de série, avec l’inscription « GT3 RS » brodée sur les appuie-tête et une bande centrale de siège en Race-Tex Noir perforé, doublée d’une couleur contrastée. En option, vous obtenez les sièges sport plus adaptatifs (18 positions, électriques). ",
+                imgPath: ""
+            },
+            {
+                title: "Plus de contraste.",
+                description: "A l’intérieur, l’exclusivité est offerte de série avec les éléments en cuir/Race-Tex en Noir, les coutures, les lettrages « GT3 RS » brodés et les sangles d’ouverture des portes en couleur contrastante Argent GT, ainsi que les baguettes décoratives noires avec inserts en carbone visible (finition satinée). Sur demande, l’intérieur est également disponible avec d’autres éléments en cuir/Race-Tex et avec les couleurs contrastantes Rouge Indien ou Argent GT. ",
+                imgPath: ""
+            },
+        ],
+    },
+]
